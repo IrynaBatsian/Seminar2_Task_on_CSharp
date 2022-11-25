361f06d01c28d7285875a105bf9bcdded3064640
Iryna <batsianiryna@gmail.com>
2022-11-25 11:57:34 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿// задача 14. Напишите программу, которая принимает на вход число и проверяет. кратно ли оно одновременно 7 и 23.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 7 == 0 && n % 23 == 0)
+    Console.Write("YES");
+else
+    Console.Write("NO");    
